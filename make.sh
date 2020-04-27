f154684132225b63e82c5c3593a299f0cffea266
Thomas Gately <20bottless@gmail.com>
2020-04-27 14:53:27 +0100

    updating bash scripts

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 counter=1
 while [ $counter -le $1 ]
 do
-	g++ ./project/tests/testxx$counter.cpp -o ./project/build/testxx$counter
+	g++ ./project/tests/testxx$counter.cpp -o ./project/build/testxx$counter.out
 	((counter++))
 done
 
--- make_test.sh
+++ make_test.sh
@@ -3,7 +3,7 @@
 counter=1
 while [ $counter -le $1 ]
 do
-	cp  ./test/defalut.cpp ./test/testxx$counter.cpp 
+	cp  ./project/test/defalut.cpp ./project/test/testxx$counter.cpp 
 	#g++ ./testxx$counter.cpp -o ./testxx$counter
 	#./testxx$counter 1 $counter
 	((counter++))
